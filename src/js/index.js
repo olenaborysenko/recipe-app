5b4e3ac4166a35aaad9bbe3cf16afaa701ce948d
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-07-03 09:50:10 +0300

    Implemented MVC model, configured first API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`${num} - ${x} = ${num-x}`);
+import axios from 'axios';
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const key = 'b604c0a01e6dce4f36198561a14cd25a';
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+getResults('pizza');
