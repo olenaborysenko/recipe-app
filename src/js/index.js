4a4e902c5f9ee52a9520702f6459f123ed1a02ab
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-08-12 19:24:40 +0300

    Implemented search results pagination

--- src/js/index.js
+++ src/js/index.js
@@ -13,29 +13,38 @@ import { elements, renderLoader, clearLoader } from './views/base';
 const state = {};
 
 const controlSearch = async () => {
-    // 1) Get query from view
-    const query = searchView.getInput();
+	// 1) Get query from view
+	const query = searchView.getInput();
 
-    if (query) {
-        // 2) New search object and add to state
-        state.search = new Search(query);
+	if (query) {
+		// 2) New search object and add to state
+		state.search = new Search(query);
 
-        // 3) Prepare UI for results
-        searchView.clearInput();
-        searchView.clearResults();
-        renderLoader(elements.searchRes);
+		// 3) Prepare UI for results
+		searchView.clearInput();
+		searchView.clearResults();
+		renderLoader(elements.searchRes);
 
-        // 4) Search for recipes
-        await state.search.getResults();
+		// 4) Search for recipes
+		await state.search.getResults();
 
-        // 5) Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
-    }
+		// 5) Render results on UI
+		clearLoader();
+		searchView.renderResults(state.search.result);
+	}
 
 }
 
 elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
+	e.preventDefault();
+	controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+	const btn = e.target.closest('.btn-inline');
+	if (btn) {
+		const goToPage = parseInt(btn.dataset.goto, 10);
+		searchView.clearResults();
+		searchView.renderResults(state.search.result, goToPage);
+	}
 });
