dedaeb323c037eddddfa26377e2f00a00c3a4969
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-08-14 23:07:32 +0300

    Completed the recipe model

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -30,4 +30,63 @@ export default class Recipe {
 	calcServings() {
 		this.servings = 4;
 	}
+
+	parseIngredients() {
+		const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds', 'drops'];
+		const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound', 'drop'];
+
+		const newIngredients = this.ingredients.map(el => {
+			// 1) Uniform units
+			let ingredient = el.toLowerCase();
+			unitsLong.forEach((unit, i) => {
+				ingredient = ingredient.replace(unit, unitsShort[i]);
+			});
+
+			// 2) Remove parentheses
+			ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+			// 3) Parse ingredients into count, unit and ingredient
+			const arrIng = ingredient.split(' ');
+			const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+
+			let objIng;
+			if (unitIndex > -1) {
+				// There is a unit
+				// Ex. 3 tsp, arrCount is [3]
+				// Ex. 3 1/2 tsp, arrCount is [3, 1/2]
+				const arrCount = arrIng.slice(0, unitIndex);
+				
+				let count;
+				if (arrCount.length === 1) {
+					count = eval(arrIng[0].replace('-', '+')); // Ex. in case of "1-1/2"
+				} else {
+					count = eval(arrIng.slice(0, unitIndex).join('+'));
+				}
+
+				objIng = {
+					count,
+					unit: arrIng[unitIndex],
+					ingredient: arrIng.slice(unitIndex + 1).join(' ')
+				}
+				
+			} else if (parseInt(arrIng[0], 10)) {
+				// There is no unit, but the 1st element is a number
+				objIng = {
+					count: parseInt(arrIng[0], 10),
+					unit: '',
+					ingredient: arrIng.slice(1).join(' ')
+				}
+			} else if (unitIndex === -1) {
+				// There is no unit and no number in the 1st position
+				objIng = {
+					count: 1,
+					unit: '',
+					ingredient
+				}
+			}
+
+			return objIng;
+		});
+		this.ingredients = newIngredients;
+	}
 }
